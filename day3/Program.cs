b6d7b36eb75714ec86598e210af39fedfa11c781
aBrickThatCodes <brickcodes@outlook.com>
2023-12-04 00:24:12 +0100

    Some more day 3 refactorings

--- day3/Program.cs
+++ day3/Program.cs
@@ -1,9 +1,8 @@
 ﻿// TODO: This could be better. Will I improve it? Maybe
 
-namespace day3;
+namespace Day3;
 
-internal static class Program
-{
+internal static class Program {
     private static void Main() {
         var lines = File.ReadAllLines("input");
         Console.WriteLine($"Part 1: {Part1(lines)}");
@@ -12,17 +11,18 @@ internal static class Program
 
     #region Part 1
 
-    private static bool IsSchematicSymbol(this char c) {
+    private static bool IsSchematicSymbol(char c) {
         return !(char.IsDigit(c) || c == '.');
     }
 
-    private static int Part1(string[] lines) {
+    private static int Part1(IReadOnlyList<string> lines) {
         var lineLength = lines[0].Length;
         var sum = 0;
-        for (var i = 0; i < lines.Length; i++) {
+        for (var i = 0; i < lines.Count; i++) {
             var beginNumber = -1;
             var leftmost = 0;
             var currentLine = lines[i];
+
             for (var j = 0; j < lineLength; j++) {
                 var currentIsDigit = char.IsDigit(currentLine[j]);
                 var lineEnd = j == lineLength - 1;
@@ -33,40 +33,40 @@ internal static class Program
                         leftmost = Math.Max(0, j - 1);
                     }
 
-                if ((!currentIsDigit || lineEnd) && beginNumber != -1) {
-                    var endNumber = currentIsDigit ? j : j - 1;
-                    var isPart = false;
+                if (currentIsDigit && !lineEnd || beginNumber == -1) continue;
 
-                    if (i > 0) {
-                        var lineAbove = lines[i - 1];
-                        for (var k = leftmost; k <= j; k++)
-                            if (IsSchematicSymbol(lineAbove[k])) {
-                                isPart = true;
-                                break;
-                            }
-                    }
+                var endNumber = currentIsDigit ? j : j - 1;
+                var isPart = false;
 
-                    if (!isPart && i < lines.Length - 1) {
-                        var lineBelow = lines[i + 1];
-                        for (var k = leftmost; k <= j; k++)
-                            if (IsSchematicSymbol(lineBelow[k])) {
-                                isPart = true;
-                                break;
-                            }
-                    }
-
-                    if (!isPart && leftmost != beginNumber) isPart = IsSchematicSymbol(currentLine[leftmost]);
-
-                    if (!isPart && endNumber != j) isPart = IsSchematicSymbol(currentLine[j]);
-
-                    if (isPart) {
-                        var s = currentLine.Substring(beginNumber, endNumber - beginNumber + 1);
-                        var n = int.Parse(s);
-                        sum += n;
-                    }
-
-                    beginNumber = -1;
+                if (i > 0) {
+                    var lineAbove = lines[i - 1];
+                    for (var k = leftmost; k <= j; k++)
+                        if (IsSchematicSymbol(lineAbove[k])) {
+                            isPart = true;
+                            break;
+                        }
                 }
+
+                if (!isPart && i < lines.Count - 1) {
+                    var lineBelow = lines[i + 1];
+                    for (var k = leftmost; k <= j; k++)
+                        if (IsSchematicSymbol(lineBelow[k])) {
+                            isPart = true;
+                            break;
+                        }
+                }
+
+                if (!isPart && leftmost != beginNumber) isPart = IsSchematicSymbol(currentLine[leftmost]);
+
+                if (!isPart && endNumber != j) isPart = IsSchematicSymbol(currentLine[j]);
+
+                if (isPart) {
+                    var s = currentLine.Substring(beginNumber, endNumber - beginNumber + 1);
+                    var n = int.Parse(s);
+                    sum += n;
+                }
+
+                beginNumber = -1;
             }
         }
 
@@ -77,8 +77,7 @@ internal static class Program
 
     #region Part 2
 
-    private class Gear
-    {
+    private class Gear {
         public int Degree = 1;
         public int Ratio;
 
@@ -87,7 +86,7 @@ internal static class Program
         }
     }
 
-    private static void AddToGear(this Dictionary<(int, int), Gear> gears, (int, int) key, int n) {
+    private static void AddToGear(this IDictionary<(int, int), Gear> gears, (int, int) key, int n) {
         if (!gears.TryGetValue(key, out var gear)) {
             gear = new Gear(n);
             gears.Add(key, gear);
@@ -99,11 +98,11 @@ internal static class Program
         }
     }
 
-    private static int Part2(string[] lines) {
+    private static int Part2(IReadOnlyList<string> lines) {
         var lineLength = lines[0].Length;
         var gears = new Dictionary<(int, int), Gear>();
 
-        for (var i = 0; i < lines.Length; i++) {
+        for (var i = 0; i < lines.Count; i++) {
             var beginNumber = -1;
             var leftmost = 0;
             var currentLine = lines[i];
@@ -117,34 +116,35 @@ internal static class Program
                         leftmost = Math.Max(0, j - 1);
                     }
 
-                if ((!currentIsDigit || lineEnd) && beginNumber != -1) {
-                    var endNumber = currentIsDigit ? j : j - 1;
-                    var s = currentLine.Substring(beginNumber, endNumber - beginNumber + 1);
-                    var n = int.Parse(s);
+                if (currentIsDigit && !lineEnd || beginNumber == -1) continue;
 
-                    if (i > 0) {
-                        var lineAbove = lines[i - 1];
-                        for (var k = leftmost; k <= j; k++)
-                            if (lineAbove[k] == '*')
-                                gears.AddToGear((i - 1, k), n);
-                    }
+                var endNumber = currentIsDigit ? j : j - 1;
+                var s = currentLine.Substring(beginNumber, endNumber - beginNumber + 1);
+                var n = int.Parse(s);
 
-                    if (i < lines.Length - 1) {
-                        var lineBelow = lines[i + 1];
-                        for (var k = leftmost; k <= j; k++)
-                            if (lineBelow[k] == '*')
-                                gears.AddToGear((i + 1, k), n);
-                    }
-
-                    if (leftmost != beginNumber)
-                        if (currentLine[leftmost] == '*')
-                            gears.AddToGear((i, leftmost), n);
-
-                    if (endNumber != j)
-                        if (currentLine[j] == '*')
-                            gears.AddToGear((i, j), n);
-                    beginNumber = -1;
+                if (i > 0) {
+                    var lineAbove = lines[i - 1];
+                    for (var k = leftmost; k <= j; k++)
+                        if (lineAbove[k] == '*')
+                            gears.AddToGear((i - 1, k), n);
                 }
+
+                if (i < lines.Count - 1) {
+                    var lineBelow = lines[i + 1];
+                    for (var k = leftmost; k <= j; k++)
+                        if (lineBelow[k] == '*')
+                            gears.AddToGear((i + 1, k), n);
+                }
+
+                if (leftmost != beginNumber)
+                    if (currentLine[leftmost] == '*')
+                        gears.AddToGear((i, leftmost), n);
+
+                if (endNumber != j)
+                    if (currentLine[j] == '*')
+                        gears.AddToGear((i, j), n);
+
+                beginNumber = -1;
             }
         }
 
