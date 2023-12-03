a62b1d84888183c3885db0685d393463357b668f
aBrickThatCodes <brickcodes@outlook.com>
2023-12-03 21:53:01 +0100

    Day 3

---
+++ day3/Program.cs
@@ -0,0 +1,199 @@
+﻿// TODO: This could be better. Will I improve it? Maybe
+static class Program
+{
+    #region Part 1
+    static bool IsSchematicSymbol(this char c)
+    {
+        return !(Char.IsDigit(c) || c == '.');
+    }
+    static int Part1(string[] lines)
+    {
+        var lineLength = lines[0].Length;
+        var sum = 0;
+        for (var i = 0; i < lines.Length; i++)
+        {
+            int beginNumber = -1, endNumber = -1, leftmost = 0;
+            var currentLine = lines[i];
+            for (var j = 0; j < lineLength; j++)
+            {
+                var currentIsDigit = Char.IsDigit(currentLine[j]);
+                var lineEnd = j == lineLength - 1;
+
+                if (currentIsDigit)
+                {
+                    if (beginNumber == -1)
+                    {
+                        beginNumber = j;
+                        leftmost = Math.Max(0, j - 1);
+                    }
+                }
+
+                if ((!currentIsDigit || lineEnd) && (beginNumber != -1 && endNumber == -1))
+                {
+                    endNumber = currentIsDigit ? j : j - 1;
+                    var isPart = false;
+
+                    if (i > 0)
+                    {
+                        var lineAbove = lines[i - 1];
+                        for (var k = leftmost; k <= j; k++)
+                        {
+                            if (IsSchematicSymbol(lineAbove[k]))
+                            {
+                                isPart = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (!isPart && i < lines.Length - 1)
+                    {
+                        var lineBelow = lines[i + 1];
+                        for (var k = leftmost; k <= j; k++)
+                        {
+                            if (IsSchematicSymbol(lineBelow[k]))
+                            {
+                                isPart = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (!isPart && leftmost != beginNumber)
+                    {
+                        isPart = IsSchematicSymbol(currentLine[leftmost]);
+                    }
+
+                    if (!isPart && endNumber != j)
+                    {
+                        isPart = IsSchematicSymbol(currentLine[j]);
+                    }
+
+                    if (isPart)
+                    {
+                        var s = currentLine.Substring(beginNumber, endNumber - beginNumber + 1);
+                        var n = Int32.Parse(s);
+                        sum += n;
+                    }
+                    beginNumber = endNumber = -1;
+                }
+            }
+        }
+        return sum;
+    }
+    #endregion
+
+    #region Part 1
+    class Gear
+    {
+        public int n = 1;
+        public int ratio;
+
+        public Gear(int n)
+        {
+            ratio = n;
+        }
+    }
+
+    static void AddToGear(this Dictionary<(int, int), Gear> gears, (int, int) key, int n)
+    {
+        if (!gears.TryGetValue(key, out Gear? gear))
+        {
+            gear = new Gear(n);
+            gears.Add(key, gear);
+        }
+        else
+        {
+            gear.n++;
+            if (gear.n < 3)
+            {
+                gear.ratio *= n;
+            }
+            gears[key] = gear;
+        }
+    }
+
+    static int Part2(string[] lines)
+    {
+        var lineLength = lines[0].Length;
+        var gears = new Dictionary<(int, int), Gear>();
+
+        for (var i = 0; i < lines.Length; i++)
+        {
+            int beginNumber = -1, endNumber = -1, leftmost = 0;
+            var currentLine = lines[i];
+            for (var j = 0; j < lineLength; j++)
+            {
+                var currentIsDigit = Char.IsDigit(currentLine[j]);
+                var lineEnd = j == lineLength - 1;
+
+                if (currentIsDigit)
+                {
+                    if (beginNumber == -1)
+                    {
+                        beginNumber = j;
+                        leftmost = Math.Max(0, j - 1);
+                    }
+                }
+
+                if ((!currentIsDigit || lineEnd) && (beginNumber != -1 && endNumber == -1))
+                {
+                    endNumber = currentIsDigit ? j : j - 1;
+                    var s = currentLine.Substring(beginNumber, endNumber - beginNumber + 1);
+                    var n = Int32.Parse(s);
+
+                    if (i > 0)
+                    {
+                        var lineAbove = lines[i - 1];
+                        for (var k = leftmost; k <= j; k++)
+                        {
+                            if (lineAbove[k] == '*')
+                            {
+                                gears.AddToGear((i - 1, k), n);
+                            }
+                        }
+                    }
+
+                    if (i < lines.Length - 1)
+                    {
+                        var lineBelow = lines[i + 1];
+                        for (var k = leftmost; k <= j; k++)
+                        {
+                            if (lineBelow[k] == '*')
+                            {
+                                gears.AddToGear((i + 1, k), n);
+                            }
+                        }
+                    }
+
+                    if (leftmost != beginNumber)
+                    {
+                        if (currentLine[leftmost] == '*')
+                        {
+                            gears.AddToGear((i, leftmost), n);
+                        }
+                    }
+
+                    if (endNumber != j)
+                    {
+                        if (currentLine[j] == '*')
+                        {
+                            gears.AddToGear((i, j), n);
+                        }
+                    }
+                    beginNumber = endNumber = -1;
+                }
+            }
+        }
+        var gearRatios = from gear in gears.Values where gear.n == 2 select gear.ratio;
+        return gearRatios.Sum();
+    }
+    #endregion
+
+    static void Main()
+    {
+        var lines = File.ReadAllLines("input");
+        Console.WriteLine($"Part 1: {Part1(lines)}");
+        Console.WriteLine($"Part 2: {Part2(lines)}");
+    }
+}
